25f4cef9edd9d5b3b478f6802084dc255ab0eeef
Your Name <4achime@gmail.com>
2025-08-26 22:58:53 +0800

    chore: remove temporary debug & ping routes

--- app/api/ping/route.ts
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-dynamic';
-export const runtime = 'nodejs';
-
-export async function GET() {
-  return new Response('pong', { 
-    status: 200, 
-    headers: { 'content-type': 'text/plain' } 
-  });
-}
