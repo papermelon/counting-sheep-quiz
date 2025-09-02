c775cbeadf35c607a461dc3f26a265c9e8ae60cf
Your Name <4achime@gmail.com>
2025-09-02 13:22:40 +0800

    style: update footer logo to use logomark.png for all screen sizes

--- components/footer-logo.tsx
+++ components/footer-logo.tsx
@@ -3,23 +3,11 @@ import Link from "next/link"
 export function FooterLogo() {
   return (
     <Link href="/" className="hover:opacity-80 transition-opacity">
-      {/* Desktop: Full logo with text */}
-      <div className="hidden md:block">
-        <img 
-          src="/images/primary-logo.png" 
-          alt="The Counting Sheep Project" 
-          className="h-16 w-auto" 
-        />
-      </div>
-      
-      {/* Mobile: Just the sheep logo */}
-      <div className="md:hidden">
-        <img 
-          src="/images/logomark.png" 
-          alt="Counting Sheep Logomark" 
-          className="h-8 w-auto" 
-        />
-      </div>
+      <img 
+        src="/images/logomark.png" 
+        alt="Counting Sheep Logomark" 
+        className="h-8 md:h-10 w-auto" 
+      />
     </Link>
   )
 }
